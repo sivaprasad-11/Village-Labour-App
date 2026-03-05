357259ec6dde9229f11b7f94c0c6af8f0b028cca
sivaprasad-11 <konkalasiva1234@gmail.com>
2026-03-05 08:37:25 +0530

    Fix frontend API to use same domain

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,27 +1,16 @@
-const API = "http://localhost:3000";
+// Same domain API (works locally + on AWS behind reverse proxy)
+const API = "";
 
 export async function getBatches() {
-
   const res = await fetch(`${API}/api/batches`);
-
   return res.json();
-
 }
 
 export async function bookBatch(data) {
-
   const res = await fetch(`${API}/api/book`, {
-
     method: "POST",
-
-    headers: {
-      "Content-Type": "application/json"
-    },
-
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
-
   });
-
   return res.json();
-
 }
